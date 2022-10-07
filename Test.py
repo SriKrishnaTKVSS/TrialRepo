893327d100123cb51846fffadd12403ce23dce71
KrishnaMaithreya <krishnamaithreya@gmail.com>
2022-10-07 23:30:14 +0530

    Added a file and updated README

---
+++ Test.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import array as arr
+
+#plotting basics
+#x=np.linspace(0,50,100)
+#lt.plot(x,np.sin(x)+np.cos(x))
+#plt.show()
+
+#msg='Hi Mr.Krishna'
+#print(msg)
+
+a=arr.array('i',[1,2,3])
+print('The new created array is : ',end='')
+for  i in range (0,3):
+    print(a[i],end='\n')
+    
